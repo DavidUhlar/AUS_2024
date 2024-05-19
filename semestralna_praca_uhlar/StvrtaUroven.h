15d46cd7233bb9c60c42a9dc43b7a69ce8d82208
DavidUhlar <dadododo02@gmail.com>
2024-05-19 19:51:14 +0200

    final odovzdanie

--- semestralna_praca_uhlar/StvrtaUroven.h
+++ semestralna_praca_uhlar/StvrtaUroven.h
@@ -2,6 +2,8 @@
 
 #include <libds/adt/sorts.h>
 #include <libds/heap_monitor.h>
+#include <algorithm>
+#include <locale>
 
 class SortComparators
 {
@@ -13,24 +15,30 @@ public:
     template<typename TypTemplate>
     void compareAlphabetical(ds::amt::ImplicitSequence<TypTemplate*>& sequenceForSort) {
         ds::adt::QuickSort<TypTemplate*> quickSorter;
+        std::locale loc;
 
         quickSorter.sort(sequenceForSort, [&](TypTemplate* a, TypTemplate* b) {
             std::string string1 = a->getStopName();
             std::string string2 = b->getStopName();
 
-            //return string1 < string2;
-            if (string1 < string2)
-            {
-                //return (-1);
-                return true;
-            }
-            if (string1 > string1)
-            {
-                //return 1;
-                return false;
-            }
-            return false;
-            });
+            return std::lexicographical_compare(
+                string1.begin(), string1.end(),
+                string2.begin(), string2.end(),
+                [&](char c1, char c2) {
+                    if (std::tolower(c1, loc) < std::tolower(c2, loc)) {
+                        //return (-1);
+                        return true;
+                    }
+                    if (std::tolower(c1, loc) > std::tolower(c2, loc)) {
+                        //return (1);
+                        return false;
+                    }
+                    return false;
+                }
+            );
+
+          
+        });
     }
 
     
